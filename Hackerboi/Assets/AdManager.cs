f61f2a431c7fc55975c22d9fef8b963f49101ba1
Pizzaslice3 <lcpiazza626@gmail.com>
2020-04-17 22:20:47 -0500

    -updated ads mechanics

--- Hackerboi/Assets/AdManager.cs
+++ Hackerboi/Assets/AdManager.cs
@@ -8,19 +8,20 @@ public class AdManager : MonoBehaviour
     public GameObject adPrefab;
     public GameObject canvas;
     public GameObject adInstance;
+    public int amountOfAds = 4;
     #endregion
-
-
 
 
     public void SpawnAds()
     {
+        for (int i = 0; i < amountOfAds; i++)
+        {
+            Vector3 newPosition = new Vector3(Random.Range(-6.4f, 6.4f), Random.Range(-3.5f, 3.5f), 0);
 
-        Vector3 newPosition = new Vector3(Random.Range(-6.4f, 6.4f), Random.Range(-3.5f, 3.5f), 0);
-
-        var newAd = Instantiate(adPrefab, newPosition, Quaternion.identity);
-        //newAd.transform.parent = canvas.transform;
-        newAd.transform.SetParent(canvas.transform);
+            var newAd = Instantiate(adPrefab, newPosition, Quaternion.identity);
+            //newAd.transform.parent = canvas.transform;
+            newAd.transform.SetParent(canvas.transform);
+        }
     }
     public void CloseAd()
     {
