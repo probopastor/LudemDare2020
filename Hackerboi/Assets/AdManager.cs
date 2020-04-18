d21aaf1270d3f4ae99e511cdcad9186594989832
Pizzaslice3 <lcpiazza626@gmail.com>
2020-04-17 21:43:00 -0500

    -sprites folder added

---
+++ Hackerboi/Assets/AdManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdManager : MonoBehaviour
+{
+    #region variables
+    public GameObject ad;
+    #endregion
+
+
+
+    public void CloseAd()
+    {
+        Vector3 position = new Vector3(Random.Range(-750.0f, 750.0f), Random.Range(-391.0f, 387.0f), 0);
+        print(position);
+        ad.transform.position = position;
+        position = Vector3.zero;
+        //ad.SetActive(false);
+    }
+
+
+
+
+
+}
