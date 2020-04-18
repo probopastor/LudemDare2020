1b8fd71c5ae8aea9dd032695dc2a128277921d86
Pizzaslice3 <lcpiazza626@gmail.com>
2020-04-18 10:05:40 -0500

    -cmd window maximizeable and minimizeable

---
+++ Hackerboi/Assets/CmdMaximize.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CmdMaximize : MonoBehaviour
+{
+    public Animator cmdAnimator;
+
+    public void ClickCmdMaximize()
+    {
+        cmdAnimator.SetTrigger("cmdMaximize");
+    }
+
+}
