4c749508d8472f8c34aed4aa322cf47c6829d3d7
Pizzaslice3 <lcpiazza626@gmail.com>
2020-04-17 21:59:42 -0500

    -adspawning mechanic works! (Kinda)

--- Hackerboi/Assets/AdManager.cs
+++ Hackerboi/Assets/AdManager.cs
@@ -6,16 +6,27 @@ public class AdManager : MonoBehaviour
 {
     #region variables
     public GameObject ad;
+    public GameObject canvas;
     #endregion
 
 
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            CloseAd();
+        }
+    }
 
     public void CloseAd()
     {
-        Vector3 position = new Vector3(Random.Range(-750.0f, 750.0f), Random.Range(-391.0f, 387.0f), 0);
-        print(position);
-        ad.transform.position = position;
-        position = Vector3.zero;
+        Vector3 newPosition = new Vector3(Random.Range(-6.4f, 6.4f), Random.Range(-3.5f, 3.5f), 0);
+
+       var newAd =  Instantiate(ad, newPosition, Quaternion.identity);
+        newAd.transform.parent = canvas.transform;
+        //print(position);
+        //ad.transform.position = position;
+        //position = Vector3.zero;
         //ad.SetActive(false);
     }
 
