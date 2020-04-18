726266e02ded6231c60a9cf1b5b3dc2716d05cc8
Pizzaslice3 <lcpiazza626@gmail.com>
2020-04-17 22:18:35 -0500

    -prefab folder created

--- Hackerboi/Assets/AdManager.cs
+++ Hackerboi/Assets/AdManager.cs
@@ -5,29 +5,27 @@ using UnityEngine;
 public class AdManager : MonoBehaviour
 {
     #region variables
-    public GameObject ad;
+    public GameObject adPrefab;
     public GameObject canvas;
+    public GameObject adInstance;
     #endregion
 
 
-    public void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            CloseAd();
-        }
-    }
 
-    public void CloseAd()
+
+    public void SpawnAds()
     {
+
         Vector3 newPosition = new Vector3(Random.Range(-6.4f, 6.4f), Random.Range(-3.5f, 3.5f), 0);
 
-       var newAd =  Instantiate(ad, newPosition, Quaternion.identity);
-        newAd.transform.parent = canvas.transform;
-        //print(position);
-        //ad.transform.position = position;
-        //position = Vector3.zero;
-        //ad.SetActive(false);
+        var newAd = Instantiate(adPrefab, newPosition, Quaternion.identity);
+        //newAd.transform.parent = canvas.transform;
+        newAd.transform.SetParent(canvas.transform);
+    }
+    public void CloseAd()
+    {
+        Destroy(adInstance);
+        
     }
 
 
