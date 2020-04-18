0a1d901fc4262982d718cfa8f0f3af6fa2c3290c
Connor <46971709+cmwolf19@users.noreply.github.com>
2020-04-18 01:17:49 -0500

    Router Table & Main Scene

---
+++ Hackerboi/Assets/Scripts/Router/SlideToRouter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+
+    public class SlideToRouter : MonoBehaviour, IPointerEnterHandler
+    {
+    public GameObject computer;
+    public GameObject router;
+    public int slideDistance;
+    public float slideSpeed;
+
+    public GameObject otherArrow;
+
+    private float myOffset = 0;
+
+    private bool sliding;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!sliding) sliding = true;
+    }
+
+    private void OnEnable()
+    {
+        myOffset = 0;
+    }
+
+    private void Update()
+    {
+        if (sliding)
+        {
+            otherArrow.SetActive(false);
+            myOffset += slideSpeed;
+            computer.transform.Translate(new Vector3(slideSpeed, 0, 0));
+            router.transform.Translate(new Vector3(slideSpeed, 0, 0));
+
+            if (Mathf.Abs(myOffset) >= slideDistance)
+            {
+                sliding = false;
+                gameObject.SetActive(false);
+                otherArrow.SetActive(true);
+            }
+        } 
+    }
+}
