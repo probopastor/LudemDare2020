a281d15f3bbe373e2e80590291160ce452b3ef6d
Pizzaslice3 <lcpiazza626@gmail.com>
2020-04-18 16:27:22 -0500

    -editing main game 1

---
+++ Hackerboi/Assets/ScoringManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class ScoringManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public int currentScore = 0;
+    
+    public void Start()
+    {
+        InvokeRepeating("IncreaseScore", 1, 1);
+    }
+
+    void IncreaseScore()
+    {
+        currentScore++;
+        scoreText.text = "<< Score Counter ->" + currentScore.ToString();
+    }
+
+
+
+
+
+}
