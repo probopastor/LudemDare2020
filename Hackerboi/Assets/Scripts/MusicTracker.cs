3e6b29590a1227170e951ed2288de74d4d2a4972
Jhyland-r <jakehyland001@gmail.com>
2020-04-18 03:34:05 -0500

    Main Menu Music and more SFX

---
+++ Hackerboi/Assets/Scripts/MusicTracker.cs
@@ -0,0 +1,29 @@
+﻿/************************************************************************************************
+// File Name:   Music Tracker.cs
+// Author:      Jake Hyland
+// Description: Contains a function for establishing the background music track being played in any given scene
+//              Takes into account any new object that plays music if it has the "Music" tag
+************************************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTracker : MonoBehaviour
+{
+
+    //Finds if the scene includes a Music playing object and destroys itself if there is new music
+    //If there isn't any new music, the object is not destroyed
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+
+        if (objs.Length > 1)
+        {
+            Destroy(objs[0]);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
