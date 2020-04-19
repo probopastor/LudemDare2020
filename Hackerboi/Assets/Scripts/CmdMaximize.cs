fdeafd35eb57508f4221dcfb8dd55d8ff676b595
probopastor <43503592+probopastor@users.noreply.github.com>
2020-04-18 19:37:16 -0500

    Revert "Fixed"
    This reverts commit 80d7f558f987e3e46f2c49db3de6eb1bc0f7e023, reversing
    changes made to b6340050e074afb42747d1ac367a41aa41938f93.

--- Hackerboi/Assets/Scripts/CmdMaximize.cs
+++ Hackerboi/Assets/Scripts/CmdMaximize.cs
@@ -7,18 +7,10 @@ public class CmdMaximize : MonoBehaviour
     public Animator cmdAnimator;
     public CmdPrompt HB;
 
-    public Animator contactsAnimator;
-
     public void ClickCmdMaximize()
     {     
         cmdAnimator.SetBool("cmdIsMaximized", true);
         cmdAnimator.SetBool("cmdIsMinimized", false);
         HB.cmdPromptOpen(true);
-    }
-
-    public void ClickContactsMaximize()
-    {
-        contactsAnimator.SetBool("ContactsEnabled", true);
-        contactsAnimator.SetBool("ContactsDisabled", false);
     }
 }
