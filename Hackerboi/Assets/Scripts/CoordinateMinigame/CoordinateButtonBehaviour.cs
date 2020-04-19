20ec28c1d61f2618a87a0e671694e4f5f0619725
sidfoe <sidfoe2125@gmail.com>
2020-04-19 16:51:53 -0400

    coordinate game

---
+++ Hackerboi/Assets/Scripts/CoordinateMinigame/CoordinateButtonBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoordinateButtonBehaviour : MonoBehaviour
+{
+    public bool isNeeded = false;
+    public bool isPressed = false;
+    public string coorLet = "";
+    public string coorNum = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Hackerboi/Assets/Scripts/CoordinateMinigame/CoordinateManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoordinateManager : MonoBehaviour
+{
+    public List<GameObject> buttons = new List<GameObject>();
+    private List<GameObject> temp = new List<GameObject>();
+    public string result = "";
+
+    public GameObject CoordinatePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 9; i++)
+        {
+            buttons.Add(CoordinatePanel.transform.GetChild(i).gameObject);
+        }
+
+        CreateErrorString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string CreateErrorString()
+    {
+        int rand = Random.Range(0, 2);
+        int amount = Random.Range(0, buttons.Count);
+        Debug.Log(amount);
+
+        int index = 0;
+
+        if (rand == 0) //letter num letter num
+        {
+            for(int i = 0; i < amount; i++)
+            {
+                temp = buttons;
+
+                for (int k = 0; k < amount; k++)
+                {
+                    index = Random.Range(0, temp.Count);
+
+                    result += buttons[index].GetComponent<CoordinateButtonBehaviour>().coorLet;
+                    result += buttons[index].GetComponent<CoordinateButtonBehaviour>().coorNum;
+
+                    temp.RemoveAt(index);
+                } 
+            }
+        }
+
+        if(rand == 1) //letter letter num num
+        {
+            string letter = "";
+            string number = "";
+
+            for (int i = 0; i < amount; i++)
+            {
+                temp = buttons;
+
+                for (int k = 0; k < amount; k++)
+                {
+                    index = Random.Range(0, temp.Count);
+
+                    letter += buttons[index].GetComponent<CoordinateButtonBehaviour>().coorLet;
+                    number += buttons[index].GetComponent<CoordinateButtonBehaviour>().coorNum;
+
+                    temp.RemoveAt(index);
+                }
+            }
+
+            result += letter + number;
+        }
+
+        Debug.Log(result);
+        return result;
+    }
+}
