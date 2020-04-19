bde159e1f3c5b44dcc88c424cc85e77c2ebcb3d3
Connor <46971709+cmwolf19@users.noreply.github.com>
2020-04-19 17:07:07 -0500

    Binary Puzzle Ready for Implementation

---
+++ Hackerboi/Assets/Scripts/Binary/BinaryController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BinaryController : MonoBehaviour
+{
+    public TextMeshProUGUI settingText;
+    public BinaryToggle[] toggles;
+    private bool[] pattern = new bool[8];
+    private bool[] compArray = new bool[8];
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i!= pattern.Length; i++)
+        {
+            pattern[i] = (Random.Range(0, 2) == 1);
+        }
+
+        foreach(bool myBool in pattern)
+        {
+            int num;
+            if (myBool) num = 1;
+            else num = 0;
+            settingText.text += num + " ";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i!= pattern.Length; i++)
+        {
+            compArray[i] = toggles[i].GetOne();
+            if (compArray[i] != pattern[i])
+            {
+                return;
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Hackerboi/Assets/Scripts/Binary/BinaryToggle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BinaryToggle : MonoBehaviour
+{
+    private bool one;
+    public TextMeshProUGUI text;
+
+    public void Toggle()
+    {
+        one = !one;
+        if (one) text.text = "1";
+        else text.text = "0";
+    }
+
+    public bool GetOne()
+    {
+        return one;
+    }
+}
