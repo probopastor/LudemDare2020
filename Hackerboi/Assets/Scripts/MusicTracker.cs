8d7b1564cfe482592ad701bfb268e12e7ef05ed0
Jhyland-r <jakehyland001@gmail.com>
2020-04-18 23:51:24 -0500

    Music script fixes and Gameplay Th3m3

--- Hackerboi/Assets/Scripts/MusicTracker.cs
+++ Hackerboi/Assets/Scripts/MusicTracker.cs
@@ -1,11 +1,4 @@
-﻿/************************************************************************************************
-// File Name:   Music Tracker.cs
-// Author:      Jake Hyland
-// Description: Contains a function for establishing the background music track being played in any given scene
-//              Takes into account any new object that plays music if it has the "Music" tag
-************************************************************************************************/
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,26 +8,17 @@ public class MusicTracker : MonoBehaviour
     //Finds if the scene includes a Music playing object and destroys the new music
     //If there isn't any new music, the object is not destroyed
 
-    void Awake()
+    public GameObject[] objs;
+
+    void Start()
     {
-        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+       GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
 
         if (objs.Length > 1)
         {
             Destroy(objs[1]);
         }
 
-        DontDestroyOnLoad(gameObject);
-    }
-
-    public void Stop() //Deletes the Current Music Playing Object after 5 seconds
-    {
-        StartCoroutine(Deletion());
-    }
-
-    public IEnumerator Deletion()
-    {
-        yield return new WaitForSeconds(6.25f);
-        Destroy(gameObject);
-    }
+        DontDestroyOnLoad(objs[0]);
+    } 
 }
