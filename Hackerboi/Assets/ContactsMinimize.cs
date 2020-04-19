404ba524232f0b2ecc6fd48c47fcc37865c5e7bb
probopastor <43503592+probopastor@users.noreply.github.com>
2020-04-18 19:41:31 -0500

    Revert "Revert "Fixed""
    This reverts commit fdeafd35eb57508f4221dcfb8dd55d8ff676b595.

---
+++ Hackerboi/Assets/ContactsMinimize.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactsMinimize : MonoBehaviour
+{
+    public Animator contactsAnimator;
+
+    public void MinimizeCmd()
+    {
+       contactsAnimator.SetBool("ContactsEnabled", false);
+       contactsAnimator.SetBool("ContactsDisabled", true);
+    }
+}
--- Hackerboi/Assets/Scripts/CmdMaximize.cs
+++ Hackerboi/Assets/Scripts/CmdMaximize.cs
@@ -7,10 +7,18 @@ public class CmdMaximize : MonoBehaviour
     public Animator cmdAnimator;
     public CmdPrompt HB;
 
+    public Animator contactsAnimator;
+
     public void ClickCmdMaximize()
     {     
         cmdAnimator.SetBool("cmdIsMaximized", true);
         cmdAnimator.SetBool("cmdIsMinimized", false);
         HB.cmdPromptOpen(true);
+    }
+
+    public void ClickContactsMaximize()
+    {
+        contactsAnimator.SetBool("ContactsEnabled", true);
+        contactsAnimator.SetBool("ContactsDisabled", false);
     }
 }
