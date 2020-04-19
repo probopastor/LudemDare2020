dc4aeb800a898b1a46ad92d14ded7b994ec74db3
probopastor <43503592+probopastor@users.noreply.github.com>
2020-04-18 23:21:28 -0500

    Working in-game timer

---
+++ Hackerboi/Assets/TimeManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class TimeManager : MonoBehaviour
+{
+    public TextMeshProUGUI timeText;
+    public TextMeshProUGUI dateText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DateTime time = DateTime.Now;
+        string hour = time.Hour.ToString();
+        string minute = time.Minute.ToString();
+
+        if(time.Minute < 10)
+        {
+            minute = "0" + time.Minute.ToString();
+        }
+
+        string day = time.Day.ToString();
+        string month = time.Month.ToString();
+        string year = time.Year.ToString();
+
+        timeText.text = hour + ":" + minute;
+        dateText.text = month + "/" + day + "/" + year;
+    }
+}
