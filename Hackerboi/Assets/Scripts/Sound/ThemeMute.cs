431df259deffd8e645c23164155eeed1ecb816cc
Jhyland-r <jakehyland001@gmail.com>
2020-04-19 12:53:55 -0500

    Transition to Gameplay theme smoother

---
+++ Hackerboi/Assets/Scripts/Sound/ThemeMute.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThemeMute : MonoBehaviour
+{
+
+    private AudioSource themeAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        themeAudio = GetComponent<AudioSource>();
+        StartCoroutine(waitVolume());
+    }
+
+    private IEnumerator waitVolume()
+    {
+        yield return new WaitForSeconds(10.3f);
+        themeAudio.volume = 0.25f;
+    }
+}
