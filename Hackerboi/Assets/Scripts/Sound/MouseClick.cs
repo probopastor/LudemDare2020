274d836706cf4b2ea94acde1f24a033844f41f95
probopastor <43503592+probopastor@users.noreply.github.com>
2020-04-20 15:02:09 -0500

    Fixed right and left arrow being "null"

--- Hackerboi/Assets/Scripts/Sound/MouseClick.cs
+++ Hackerboi/Assets/Scripts/Sound/MouseClick.cs
@@ -7,8 +7,8 @@ public class MouseClick : MonoBehaviour
 {
     private AudioSource clickSource;
     public AudioClip[] clicking;
-    private GameObject rightArrow;
-    private GameObject leftArrow;
+    public GameObject rightArrow;
+    public GameObject leftArrow;
     private Scene scene;
 
     // Start is called before the first frame update
@@ -18,8 +18,8 @@ public class MouseClick : MonoBehaviour
         scene = SceneManager.GetActiveScene();
         if (scene.name == "MainGame")
         {
-            rightArrow = GameObject.Find("ArrowToRouter");
-            leftArrow = GameObject.Find("ArrowToDesktop");
+            //rightArrow = GameObject.Find("ArrowToRouter");
+            //leftArrow = GameObject.Find("ArrowToDesktop");
         }
     }
 
@@ -28,7 +28,7 @@ public class MouseClick : MonoBehaviour
     {
         if (scene.name == "MainGame")
         {
-            if (Input.GetMouseButtonDown(0) && rightArrow.active)
+            if (Input.GetMouseButtonDown(0) && rightArrow.activeSelf)
             {
                 clickSource.PlayOneShot(clicking[Random.Range(0, clicking.Length)], 1.8F);
             }
