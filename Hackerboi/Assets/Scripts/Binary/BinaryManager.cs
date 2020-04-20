2f9aa2b6ca183297b9bb6bf200b2480152b6679f
Jhyland-r <jakehyland001@gmail.com>
2020-04-20 17:22:08 -0500

    Binary Hack Appearing Noise

--- Hackerboi/Assets/Scripts/Binary/BinaryManager.cs
+++ Hackerboi/Assets/Scripts/Binary/BinaryManager.cs
@@ -8,11 +8,20 @@ public class BinaryManager : Problem
     private GameObject curPrefab;
     public GameObject canvas;
 
+    private AudioSource binSource;
+    public AudioClip appear;
+
+    public void Start()
+    {
+        binSource = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>();
+    }
+
     public override void CauseProblem()
     {
         if (curPrefab == null)
             curPrefab = Instantiate(prefab, canvas.transform);
         else curPrefab.SetActive(true);
+        binSource.PlayOneShot(appear);
     }
 
     public override void SolveProblem()
