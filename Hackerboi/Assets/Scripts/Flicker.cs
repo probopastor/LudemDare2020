3a9035f0f1bcdaa8e464b059b72c2691e0dd7011
Connor <46971709+cmwolf19@users.noreply.github.com>
2020-04-20 15:27:35 -0500

    Gameover cursor

---
+++ Hackerboi/Assets/Scripts/Flicker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Flicker : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Toggle", 0, 2);
+    }
+
+    void Toggle()
+    {
+        text.enabled = !text.enabled;
+    }
+
+}
