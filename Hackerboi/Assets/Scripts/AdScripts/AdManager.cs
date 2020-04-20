5bb7d1d3951b096f37d0611034bb72e8d4004215
probopastor <43503592+probopastor@users.noreply.github.com>
2020-04-20 02:58:33 -0500

    Methods that make ads appear

--- Hackerboi/Assets/Scripts/AdScripts/AdManager.cs
+++ Hackerboi/Assets/Scripts/AdScripts/AdManager.cs
@@ -4,22 +4,146 @@ using UnityEngine;
 
 public class AdManager : MonoBehaviour
 {
-    #region variables
-    public GameObject adPrefab;
-    public GameObject canvas;
-    public int amountOfAds = 4;
-    #endregion
+    public GameObject[] ads;
+    public List<int> selectedAds = new List<int>();
+
+    private int randomAmountOfAds = 0;
+    private int randomAd = 0;
+
+    private bool adChosen;
+
+    public GameObject[] henkAds;
+    public GameObject[] bigEarsAds;
+    public GameObject[] needAFixAds;
+    public GameObject[] spearsAds;
+    public GameObject[] blindsAds;
+    public GameObject[] raveAds;
 
 
-    public void SpawnAds()
+    private void Start()
     {
-        for (int i = 0; i < amountOfAds; i++)
-        {
-            Vector3 newPosition = new Vector3(Random.Range(-6.4f, 6.4f), Random.Range(-3.5f, 3.5f), 0);
+        adChosen = true;
 
-            var newAd = Instantiate(adPrefab, newPosition, Quaternion.identity);
-            
-            newAd.transform.SetParent(canvas.transform);
+        for (int i = 0; i < ads.Length; i++)
+        {
+            ads[i].SetActive(false);
         }
     }
- }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SpawnAds();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            HenkAdSpam();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            BigEarsAdSpam();
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            NeedAFixSpam();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            SpearAdSpam();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            BlindsAdSpam();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            RaveAdSpam();
+        }
+    }
+    
+    public void SpawnAds()
+    {
+        randomAmountOfAds = Random.Range(0, ads.Length + 1);
+
+        for(int i = 0; i < randomAmountOfAds; i++)
+        {
+            StartCoroutine(EnableAds());
+        }
+    }
+
+    private IEnumerator EnableAds()
+    {
+        randomAd = Random.Range(0, randomAmountOfAds + 1);
+
+        if(selectedAds.Count > 0)
+        {
+            for(int x = 0; x < selectedAds.Count; x++)
+            {
+                if(selectedAds[x] == randomAd)
+                {
+                    adChosen = false;
+                }
+            }
+        }
+
+        if(adChosen)
+        {
+            ads[randomAd].SetActive(true);
+        }
+        else if(!adChosen)
+        {
+            yield return new WaitForEndOfFrame();
+            StartCoroutine(EnableAds());
+        }
+
+        yield return new WaitForEndOfFrame();
+    }
+
+    public void HenkAdSpam()
+    {
+        for(int i = 0; i < henkAds.Length; i++)
+        {
+            henkAds[i].SetActive(true);
+        }
+    }
+
+    public void BigEarsAdSpam()
+    {
+        for (int i = 0; i < bigEarsAds.Length; i++)
+        {
+            bigEarsAds[i].SetActive(true);
+        }
+    }
+
+    public void NeedAFixSpam()
+    {
+        for (int i = 0; i < needAFixAds.Length; i++)
+        {
+            needAFixAds[i].SetActive(true);
+        }
+    }
+
+    public void SpearAdSpam()
+    {
+        for (int i = 0; i < spearsAds.Length; i++)
+        {
+            spearsAds[i].SetActive(true);
+        }
+    }
+
+    public void BlindsAdSpam()
+    {
+        for (int i = 0; i < blindsAds.Length; i++)
+        {
+            blindsAds[i].SetActive(true);
+        }
+    }
+
+    public void RaveAdSpam()
+    {
+        for (int i = 0; i < raveAds.Length; i++)
+        {
+            raveAds[i].SetActive(true);
+        }
+    }
+}
